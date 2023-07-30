fe2b742b33ccf26bb6442e45a245345bab738130
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-07-30 19:14:38 +0000

    Tidyups

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+
+FROM python:3.10-slim-bullseye
+ENV PYTHONUNBUFFERED 1
+RUN apt-get update \
+    && apt-get install -y make \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /app
+WORKDIR /app
+COPY requirements.txt /app
+RUN python3 -m venv .
+RUN python3 -m pip install pip==23.2.1
+RUN python3 -m pip install setuptools==68.2.2
+RUN python3 -m pip install --no-cache-dir -r requirements.txt
+COPY ./app /app
+EXPOSE 5000
+CMD ["gunicorn", "--bind", ":5000", "server:app"]
---
+++ app/app/__init__.py
@@ -0,0 +1,35 @@
+
+"""
+__init__.py: Flask server with Deep Learning model.
+"""
+
+__author__      = "alvertogit"
+__copyright__   = "Copyright 2018-2020"
+
+
+from flask import Flask, render_template
+
+from config import config
+from .model import init_model
+
+def create_app(config_name="default"):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    with app.app_context():
+        app.config["model"] = init_model()
+
+    from .api import api
+    app.register_blueprint(api, url_prefix="/api")
+
+    @app.route("/dlflask", methods=["GET"])
+    def dlflask():
+        return render_template("dlflask.html")
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return "Deep Learning on Flask"
+
+    return app
