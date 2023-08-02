54d803ac7f8b6edd3cf0744cad9f96b64d45aa5c
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-08-02 07:57:35 +0000

    mpt2sas: Don't disable device twice at suspend.

--- app/app/api.py
+++ app/app/api.py
@@ -6,4 +6,14 @@ __author__      = "alvertogit"
 __copyright__   = "Copyright 2018-2023"
 
 
-from flask import Blueprint, jso
+from flask import Blueprint, jsonify, request
+
+from skimage.io import imread
+import io
+
+from .model import *
+
+
+api = Blueprint('api', __name__)
+
+@api.route("/
