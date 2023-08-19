be736dea3626f922cf8660f01568edb3ae3ba33e
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-08-19 15:49:14 +0000

    Add setup.cfg

--- app/app/model.py
+++ app/app/model.py
@@ -2,4 +2,9 @@
 model.py: Functions related to Deep Learning model based on Keras.
 """
 
-__author__     
+__author__      = "alvertogit"
+__copyright__   = "Copyright 2018-2023"
+
+
+from tensorflow.keras.models import load_model
+from skimage import transf
