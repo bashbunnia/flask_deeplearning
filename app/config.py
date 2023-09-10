d73954f2ed8090ca0a7c89c19c857764722a6735
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-09-10 05:28:36 +0000

    Create seedbot.lua

---
+++ app/config.py
@@ -0,0 +1,26 @@
+
+"""
+config.py: Configurations used by Flask server.
+"""
+
+__author__      = "alvertogit"
+__copyright__   = "Copyright 2018-2023"
+
+
+import os
+
+class DefaultConfig:
+    if os.environ.get("SECRET_KEY"):
+        SECRET_KEY = os.environ.get("SECRET_KEY")
+    else:
+        raise ValueError("SECRET KEY NOT FOUND!")
+
+    MODEL_PATH = os.environ.get("MODEL_PATH") or "/app/mnist_model.h5"
+
+    @staticmethod
+    def init_app(app):
+        print("PRODUCTION CONFIG")
+
+
+class DevConfig(DefaultConfig):
+    DEBUG = True
