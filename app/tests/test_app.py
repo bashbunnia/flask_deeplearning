a35364e520db68d61dba38fe50d5746e8474e4f9
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-09-16 13:12:04 +0000

    ng

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,2 +1,13 @@
 """
 test_app.py: It contents flask app tests.
+"""
+
+__author__      = "alvertogit"
+__copyright__   = "Copyright 2018-2023"
+
+
+import pytest
+import json
+import sys
+
+def test_index(cl
