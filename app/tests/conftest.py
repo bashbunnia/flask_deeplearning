7b074316c6559241e26433f54c3a67a84074cc24
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-09-16 07:19:02 +0000

    Update KaFanList.txt

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,4 +3,12 @@ conftest.py: It contents fixture functions used in tests.
 """
 
 __author__      = "alvertogit"
-__
+__copyright__   = "Copyright 2018-2023"
+
+
+from app import create_app
+import pytest
+
+@pytest.fixture
+def app():
+    app = create
