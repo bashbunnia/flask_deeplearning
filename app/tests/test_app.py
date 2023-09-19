ecb20f4604d1555fbce2c0a74dd6dcaea0fee93b
bashbunnia <131359895+bashbunnia@users.noreply.github.com>
2023-09-19 19:25:16 +0000

    change resume

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -14,4 +14,8 @@ def test_index(client):
     response = client.get("/")
     # check response
     assert response.status_code == 200
-    assert response.data == b"Deep Learn
+    assert response.data == b"Deep Learning on Flask"
+
+def test_api(client):
+    # server REST API endpoint url and example image path
+    SERVER_URL = "http://127.0.0.1:5000/api/pred
